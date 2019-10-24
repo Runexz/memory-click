17fb001dee9874baf4382397e84a76369eb9d00f
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-23 20:07:03 -0500

    added navbar and jumbotron

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,23 @@
 // Header.js
+
+import React from "react";
+import "./Header.css"
+
+const Header = props => (
+
+    <div>
+    <nav className="navbar bg-dark justify-content-center">
+  <span className="navbar-text text-white h3">
+    Current Game Score: {props.currentScore} | Top Score: {props.topScore}
+  </span>
+  </nav>
+  <div class="jumbotron text-center">
+  <div class="container">
+    <h1 class="display-4">Memory Click Game</h1>
+    <p class="lead">Click an image but dont click it again or you restart!</p>
+  </div>
+</div>
+  </div>
+);
+
+export default Header;
