820eecc84daf4b4d98d6d29b036f9ee233a7dc6f
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-23 20:06:07 -0500

    imported card, wrapper, header, footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+
+import Card from './components/Card';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Import the cards from json list that has images links in it.
+import cardImages from "./cardImages.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      <Header />
+  )
 }
 
 export default App;
