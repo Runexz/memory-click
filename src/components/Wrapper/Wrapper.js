6c8bb6be8a0caee56fba968bf3db5711e8615785
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-23 20:08:03 -0500

    created container for card images

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,1 +1,10 @@
 // Wrapper.js
+
+import React from "react";
+import "./Wrapper.css";
+
+const Wrapper = props => (
+    <div className="container wrapper">{props.children}</div>
+);
+
+export default Wrapper;
