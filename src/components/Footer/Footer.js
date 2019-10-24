f9e03a20e56e39256c92e931118fd9c01e6dc20c
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-23 21:30:19 -0500

    footer created and exported

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,12 @@
 // Footer.js
+
+import React from "react";
+import "./Footer.css";
+
+const Footer = props => (
+    <div className="footer text-center bg-danger h5">
+        <p>Ricardo Rodriguez &copy; 2019</p>
+    </div>
+)
+
+export default Footer;
