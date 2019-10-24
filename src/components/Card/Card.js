33828f0248dc191611f1e074328fdd37d750f078
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-23 21:29:38 -0500

    Card Prop added and exported

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,1 +1,18 @@
 // Card.js
+
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+    <div className="card">
+        <div className="img-container">
+            <img
+            alt={"This is image " + props.id}
+            src={props.image}
+            onClick={() => props.checkIfClicked(props.id)}
+            />
+        </div>
+    </div>
+);
+
+export default Card;
