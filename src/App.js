1a1ae27afe67717872d125dd62fcb8b0a552ab85
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-23 21:29:00 -0500

    sets scores and checks cards to see if clicked

--- src/App.js
+++ src/App.js
@@ -8,11 +8,76 @@ import Footer from './components/Footer';
 // Import the cards from json list that has images links in it.
 import cardImages from "./cardImages.json";
 
-function App() {
-  return (
-    
-      <Header />
-  )
+// Creates constructor props from cards to set the top and current score to 0
+// topScore is the most cards clicked without being clicked again
+// currentScore is the most clicked this round without being clicked again and will reset to 0 if duplicated
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardImages: cardImages,
+      topScore: 0,
+      currentScore: 0,
+    };
+    this.checkIfClicked = this.checkIfClicked.bind(this);
+  }
+
+  // This function will see if a card has been clicked
+  checkIfClicked(id) {
+
+    // Create a copy of the cards with a random math method to sort the array
+    let clickedCard = this.state.cardImages.filter(card => card.id === id)[0];
+    let cardsCopy = this.state.cardImages.slice().sort(function (a, b) { return 0.5 - Math.random() });
+
+    // If a card has not been clicked then set its click state to true
+    if (!clickedCard.clicked) {
+      clickedCard.clicked = true;
+      cardsCopy[cardsCopy.findIndex((card) => card.id === id)] = clickedCard;
+
+      // Set the STATE and increments the counter
+      this.setState({
+        cardImages: cardsCopy,
+        currentScore: this.state.currentScore + 1,
+        topScore: (this.state.currentScore + 1 > this.state.topScore ? this.state.currentScore + 1 : this.state.topScore),
+      });
+    }
+
+    // If a card has already been clicked then set its click to false and reset the game
+    else {
+      alert("You clicked the same image! Start over!");
+      let resetCardsCopy = cardsCopy.map((card) => {
+        return {
+          id: card.id,
+          image: card.image,
+          clicked: false,
+        }
+      });
+      this.setState({
+        cardImages: resetCardsCopy,
+        currentScore: 0,
+      });
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header currentScore={this.state.currentScore} topScore={this.state.topScore} />
+        <Wrapper>
+          {this.state.cardImages.map(card => (
+            <Card
+              checkIfClicked={this.checkIfClicked}
+              id={card.id}
+              key={card.id}
+              image={card.image}
+            />
+          ))}
+        </Wrapper>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
