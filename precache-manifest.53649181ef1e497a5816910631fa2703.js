003d53be55a8cef8b196a0826c02241e7ffe3236
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-25 13:23:14 -0500

    Updates

---
+++ precache-manifest.53649181ef1e497a5816910631fa2703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b30dd9a927839effa9cac0e647fb195",
+    "url": "/memory-click/index.html"
+  },
+  {
+    "revision": "3ebac3ad275e9dc77a9d",
+    "url": "/memory-click/static/css/main.2120462b.chunk.css"
+  },
+  {
+    "revision": "6cf58c43b2e5bdaf5fd6",
+    "url": "/memory-click/static/js/2.4ec65fc3.chunk.js"
+  },
+  {
+    "revision": "3ebac3ad275e9dc77a9d",
+    "url": "/memory-click/static/js/main.80d1a527.chunk.js"
+  },
+  {
+    "revision": "f52dbd1db91d138aa683",
+    "url": "/memory-click/static/js/runtime-main.c7fc3818.js"
+  }
+]);
